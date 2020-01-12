6718573f4621f649d53741bd6db5f19a6f62d8ee
Pouya Kary <pmk@PouyasMacBookPro.local>
2020-01-12 12:51:14 +0330

    updated the installation script

--- nota/install.sh
+++ nota/install.sh
@@ -2,3 +2,14 @@ curl -sSL https://get.haskellstack.org/ | sh
 git clone http://codes.kary.us/nota/nota.git
 cd nota
 make install
+
+
+clear
+echo ""
+echo "    ┌──────────────────────────────────────┐"
+echo "    │                                      │"
+echo "    │  Nota is installed! Use the 'nota'   │"
+echo "    │        command to launch it!         │"
+echo "    │                                      │"
+echo "    └──────────────────────────────────────┘"
+echo ""
